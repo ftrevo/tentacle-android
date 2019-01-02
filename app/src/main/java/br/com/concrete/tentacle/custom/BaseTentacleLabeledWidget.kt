ad0d09d0750f1526298f68d509901c83f1296329
leo.malkes <leo.malkes@gmail.com>
2019-01-02 13:07:50 -0300

    Issue id: 16

--- app/src/main/java/br/com/concrete/tentacle/custom/BaseTentacleLabeledWidget.kt
+++ app/src/main/java/br/com/concrete/tentacle/custom/BaseTentacleLabeledWidget.kt
@@ -4,26 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
-import android.widget.TextView
-import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.tentacle_edit_text_layout.view.*
 
 abstract class BaseTentacleLabeledWidget(context: Context, attrs: AttributeSet): LinearLayout(context, attrs) {
 
-
     fun showError(show: Boolean){
         tvError.visibility = if (show) View.VISIBLE else View.INVISIBLE
-    }
-
-    fun getEditText(): TextInputEditText{
-        return edt
-    }
-
-    fun getLabel(): TextView{
-        return tvLabel
-    }
-
-    fun getErrorLabel(): TextView{
-        return tvError
     }
 }
