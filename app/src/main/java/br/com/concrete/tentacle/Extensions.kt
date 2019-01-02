d95dd5834a5aa5a465145c297b95f11e8494ccfa
leo.malkes <leo.malkes@gmail.com>
2019-01-02 11:38:54 -0300

    Issue id: 7

--- app/src/main/java/br/com/concrete/tentacle/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package br.com.concrete.tentacle
-
-import com.google.android.material.textfield.TextInputLayout
-
-fun TextInputLayout.resError(error: Int){
-    this.error = this.context.getString(error)
-}
-
--- app/src/main/java/br/com/concrete/tentacle/utils/LogWrapper.kt
+++ app/src/main/java/br/com/concrete/tentacle/utils/LogWrapper.kt
@@ -4,8 +4,8 @@ import android.util.Log
 import br.com.concrete.tentacle.BuildConfig
 
 object LogWrapper {
-    fun log(tag: String, msg: String){
-        if(BuildConfig.DEBUG){
+    fun log(tag: String, msg: String) {
+        if (BuildConfig.DEBUG) {
             Log.d(tag, msg)
         }
     }
