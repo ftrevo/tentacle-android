d7f879a98d1d49ca58b1360416cb5ac8ed1f79c1
diogo ribeiro <dgr@cin.ufpe.br>
2019-01-15 15:49:10 -0300

    Token authorization

--- app/src/main/java/br/com/concrete/tentacle/di/NetworkModule.kt
+++ app/src/main/java/br/com/concrete/tentacle/di/NetworkModule.kt
@@ -1,8 +1,13 @@
 package br.com.concrete.tentacle.di
 
+import android.content.SharedPreferences
 import br.com.concrete.tentacle.BuildConfig
+import br.com.concrete.tentacle.data.models.Session
 import br.com.concrete.tentacle.data.network.ApiService
+import br.com.concrete.tentacle.data.repositories.SharedPrefRepository
+import br.com.concrete.tentacle.utils.PREFS_KEY_USER_SESSION
 import com.google.gson.Gson
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module.module
@@ -13,6 +18,7 @@ import java.util.concurrent.TimeUnit
 
 private const val CONNECTION_TIMEOUT = 15L
 private const val READ_TIMEOUT = 30L
+private const val TOKEN_AUTHORIZATION = "Authorization"
 
 const val PROPERTY_BASE_URL = "PROPERTY_BASE_URL"
 
@@ -27,10 +33,26 @@ val networkModule = module {
             httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
         }
 
+        val tokenInterceptor = Interceptor { chain ->
+            val prefs: SharedPrefRepository = get()
+            val userSession = prefs.getStoredSession(PREFS_KEY_USER_SESSION)
+
+            if (userSession != null) {
+                val newRequest = chain.request()
+                    .newBuilder()
+                    .header(TOKEN_AUTHORIZATION, "${userSession.tokenType} ${userSession.accessToken}")
+                    .build()
+                chain.proceed(newRequest)
+            } else {
+                chain.proceed(chain.request())
+            }
+        }
+
         OkHttpClient.Builder()
             .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
             .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
             .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(tokenInterceptor)
             .build()
     }
 
