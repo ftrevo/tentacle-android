65f693bbc0edd2ea06359768e0053072117e083a
Leonardo Malkes <leo.malkes@gmail.com>
2019-01-16 10:40:25 +0000

    Navigation (WIP)

--- app/src/main/java/br/com/concrete/tentacle/features/HostActivity.kt
+++ app/src/main/java/br/com/concrete/tentacle/features/HostActivity.kt
@@ -2,6 +2,7 @@ package br.com.concrete.tentacle.features
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.utils.LogWrapper
 import kotlinx.android.synthetic.main.activity_host.bottomBar
@@ -15,11 +16,15 @@ class HostActivity : AppCompatActivity() {
         bottomBar.startListener { action ->
             when (action) {
                 R.id.action_library -> LogWrapper.log("ACTION", "Library")
-                R.id.action_games -> LogWrapper.log("ACTION", "Games")
-                R.id.action_home -> LogWrapper.log("ACTION", "Home")
+                R.id.action_games -> navigateTo(R.id.navigate_to_my_games)
+                R.id.action_home -> navigateTo(R.id.navigate_to_home)
                 R.id.action_reservation -> LogWrapper.log("ACTION", "Wallet")
                 R.id.action_events -> LogWrapper.log("ACTION", "Calendar")
             }
         }
+    }
+
+    private fun navigateTo(destination: Int){
+        Navigation.findNavController(this@HostActivity, R.id.host_fragment).navigate(destination)
     }
 }
