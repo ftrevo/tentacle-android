b58e71a8769424c68fa0f7a4c0a40189d41e063c
diogo ribeiro <dgr@cin.ufpe.br>
2019-01-24 18:29:42 -0300

    Intrumented tests configuration

---
+++ app/src/androidTest/java/br/com/concrete/tentacle/TentacleTestApp.kt
@@ -0,0 +1,29 @@
+package br.com.concrete.tentacle
+
+import PROPERTY_BASE_URL
+import android.app.Application
+import br.com.concrete.tentacle.di.repositoryModule
+import br.com.concrete.tentacle.di.sharedPreferencesModule
+import br.com.concrete.tentacle.di.viewModelModule
+import networkModule
+import org.koin.android.ext.android.startKoin
+
+private const val LOCALHOST_URL = "http://localhost:8080/"
+
+class TentacleTestApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin(this,
+            listOf(
+                networkModule,
+                viewModelModule,
+                repositoryModule,
+                sharedPreferencesModule
+            ),
+
+            extraProperties = mapOf(PROPERTY_BASE_URL to LOCALHOST_URL))
+    }
+
+}
