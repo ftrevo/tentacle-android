afea3e3642433f6a5ff513c80169ad69e6798202
Silva <maherbson.d.silva@accenture.com>
2019-01-24 11:44:14 -0300

    Criação e implementação do TestRunner

--- app/src/androidTest/java/br/com/concrete/tentacle/util/TentacleTestRunner.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/util/TentacleTestRunner.kt
@@ -2,13 +2,20 @@ package br.com.concrete.tentacle.util
 
 import android.app.Application
 import android.content.Context
+import android.os.AsyncTask
 import androidx.test.runner.AndroidJUnitRunner
 import br.com.concrete.tentacle.TestApp
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
 
 class TentacleTestRunner: AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
         return super.newApplication(cl, TestApp::class.java.name, context)
     }
 
-
+    override fun onStart() {
+        super.onStart()
+        RxJavaPlugins.setInitComputationSchedulerHandler { Schedulers.from(AsyncTask.THREAD_POOL_EXECUTOR) }
+        RxJavaPlugins.setInitIoSchedulerHandler { Schedulers.from(AsyncTask.THREAD_POOL_EXECUTOR) }
+    }
 }
