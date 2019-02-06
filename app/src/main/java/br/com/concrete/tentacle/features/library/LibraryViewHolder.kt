4b11039461fcb803fb8bc6d4ed1fba10626cf2da
daividvleal <daivid.v.leal@gmail.com>
2019-02-06 18:36:22 -0300

    Login Instrumented Test done

--- app/src/main/java/br/com/concrete/tentacle/features/library/LibraryViewHolder.kt
+++ app/src/main/java/br/com/concrete/tentacle/features/library/LibraryViewHolder.kt
@@ -1,14 +1,20 @@
 package br.com.concrete.tentacle.features.library
 
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.data.models.library.Library
 import br.com.concrete.tentacle.data.models.library.MediaLibrary
 import br.com.concrete.tentacle.extensions.animation
-import kotlinx.android.synthetic.main.library_item_layout.view.*
+import kotlinx.android.synthetic.main.library_item_layout.view.group
+import kotlinx.android.synthetic.main.library_item_layout.view.ivArrow
+import kotlinx.android.synthetic.main.library_item_layout.view.tv360
+import kotlinx.android.synthetic.main.library_item_layout.view.tv3DS
+import kotlinx.android.synthetic.main.library_item_layout.view.tvGameName
+import kotlinx.android.synthetic.main.library_item_layout.view.tvNS
+import kotlinx.android.synthetic.main.library_item_layout.view.tvONE
+import kotlinx.android.synthetic.main.library_item_layout.view.tvPS3
+import kotlinx.android.synthetic.main.library_item_layout.view.tvPS4
 
 class LibraryViewHolder(
     private val mLinearLayout: View
@@ -16,12 +22,13 @@ class LibraryViewHolder(
 
     companion object {
         private var viewStateOpen = false
+
         fun callBack(holder: RecyclerView.ViewHolder, element: Library) {
             if (holder is LibraryViewHolder) {
                 holder.mLinearLayout.tvGameName.text = element.title
 
                 holder.mLinearLayout.ivArrow.setOnClickListener {
-                    if (viewStateOpen) animateClose(holder.mLinearLayout) else animateOpen(holder.mLinearLayout, element)
+                    if (this.viewStateOpen) animateClose(holder.mLinearLayout) else animateOpen(holder.mLinearLayout, element)
                     viewStateOpen = !viewStateOpen
                 }
             }
