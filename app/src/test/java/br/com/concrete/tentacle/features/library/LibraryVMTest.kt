0833c08fd62f3ea04c5048429b48b3bc4e213a72
Maherbson Silva Dantas <maherbson@gmail.com>
2019-02-15 14:38:18 -0300

    Implementando teste filter

--- app/src/test/java/br/com/concrete/tentacle/features/library/LibraryVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/library/LibraryVMTest.kt
@@ -6,6 +6,8 @@ import br.com.concrete.tentacle.data.models.ErrorResponse
 import br.com.concrete.tentacle.data.models.ViewStateModel
 import br.com.concrete.tentacle.data.models.library.Library
 import br.com.concrete.tentacle.data.models.library.LibraryResponse
+import br.com.concrete.tentacle.data.models.library.filter.SubItem
+import br.com.concrete.tentacle.utils.QueryUtils
 import com.google.common.reflect.TypeToken
 import com.google.gson.GsonBuilder
 import okhttp3.mockwebserver.MockResponse
@@ -87,6 +89,44 @@ class LibraryVMTest : BaseViewModelTest() {
         libraryViewModel.getLibrary().observeForever {
             actual = it
         }
+        libraryViewModel.loadLibrary()
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `when libraryViewModel calls loadlibrary should return a list of Library after filter`() {
+        val filtersSelected = ArrayList<SubItem>()
+        filtersSelected.add(SubItem("Plataformas", "PS4", "Playstation 4", true))
+
+        val queryParameters = QueryUtils.assemblefilterQuery(filtersSelected)
+
+        libraryViewModel.loadLibrary(queryParameters)
+
+        val responseJson = getJson(
+            "mockjson/library/get_library_after_filter_success.json"
+        )
+
+        val collectionType = object : TypeToken<BaseModel<LibraryResponse>>() {}.type
+        val responseObject: BaseModel<LibraryResponse> = GsonBuilder()
+            .create()
+            .fromJson(responseJson, collectionType)
+
+        val expected =
+            ViewStateModel(
+                status = ViewStateModel.Status.SUCCESS,
+                model = responseObject.data.list)
+        var actual = ViewStateModel<ArrayList<Library>>(status = ViewStateModel.Status.LOADING)
+
+        val mockResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody(responseJson)
+
+        mockServer.enqueue(mockResponse)
+
+        libraryViewModel.getLibrary().observeForever {
+            actual = it
+        }
+
         libraryViewModel.loadLibrary()
         Assert.assertEquals(expected, actual)
     }
