972464902f1c478a03c70a8d2ffc71dcc681460c
daividvleal <daivid.v.leal@gmail.com>
2019-02-19 20:08:01 -0300

    instrumented test library done

--- app/src/androidTest/java/br/com/concrete/tentacle/features/library/LibraryFragmentTest.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/features/library/LibraryFragmentTest.kt
@@ -1,11 +1,19 @@
 package br.com.concrete.tentacle.features.library
 
+import android.widget.EditText
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.base.BaseFragmentTest
 import br.com.concrete.tentacle.extensions.getJson
+import br.com.concrete.tentacle.extensions.waitUntil
 import okhttp3.mockwebserver.MockResponse
 import org.hamcrest.CoreMatchers
 import org.junit.Test
@@ -26,14 +34,14 @@ class LibraryFragmentTest : BaseFragmentTest() {
                 .setBody(response)
         )
 
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerListError))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerListView))
-            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
-        Espresso.onView(ViewMatchers.withId(R.id.errorDescription))
-            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.no_library_error)))
-        Espresso.onView(ViewMatchers.withId(R.id.progressBarList))
-            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
+        onView(withId(R.id.recyclerListError))
+            .check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.recyclerListView))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+        onView(withId(R.id.errorDescription))
+            .check(ViewAssertions.matches(withText(R.string.no_library_error)))
+        onView(withId(R.id.progressBarList))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
     }
 
     @Test
@@ -45,12 +53,12 @@ class LibraryFragmentTest : BaseFragmentTest() {
                 .setBody(response)
         )
 
-        Espresso.onView(ViewMatchers.withId(R.id.progressBarList))
-            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerListView))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerListError))
-            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
+        onView(withId(R.id.progressBarList))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+        onView(withId(R.id.recyclerListView))
+            .check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.recyclerListError))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
     }
 
     @Test
@@ -59,13 +67,113 @@ class LibraryFragmentTest : BaseFragmentTest() {
             .setBody("mockjson/errors/error_400.json".getJson())
             .setResponseCode(400))
 
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerListView))
-            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
-        Espresso.onView(ViewMatchers.withId(R.id.progressBarList))
-            .check(ViewAssertions.matches(CoreMatchers.not(ViewMatchers.isDisplayed())))
-        Espresso.onView(ViewMatchers.withId(R.id.recyclerListError))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(ViewMatchers.withId(R.id.errorDescription))
-            .check(ViewAssertions.matches(ViewMatchers.withText(R.string.load_library_error_not_know)))
+        onView(withId(R.id.recyclerListView))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+        onView(withId(R.id.progressBarList))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+        onView(withId(R.id.recyclerListError))
+            .check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.errorDescription))
+            .check(ViewAssertions.matches(withText(R.string.load_library_error_not_know)))
+    }
+
+    @Test
+    fun showListWhenTypeFourLetters() {
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/library/get_library_success.json".getJson()))
+
+        onView(withId(R.id.action_search)).perform(click())
+
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/library/get_library_filtered_success.json".getJson()))
+
+        onView(ViewMatchers.isAssignableFrom(EditText::class.java))
+            .perform(ViewActions.typeText("FIFA"))
+
+        onView(withId(R.id.recyclerListView))
+            .perform(isDisplayed().waitUntil())
+        onView(withId(R.id.recyclerListError))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+    }
+
+    @Test
+    fun showEmptyStateAfterSearch() {
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/library/get_library_success.json".getJson()))
+
+        onView(withId(R.id.action_search)).perform(click())
+
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/common/common_empty_list_success.json".getJson()))
+
+        onView(ViewMatchers.isAssignableFrom(EditText::class.java))
+            .perform(ViewActions.typeText("FIFA"))
+
+        onView(withId(R.id.recyclerListError))
+            .perform(isDisplayed().waitUntil())
+        onView(withId(R.id.recyclerListView))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+        onView(withId(R.id.errorDescription))
+            .check(ViewAssertions.matches(withText(R.string.first_game_register)))
+    }
+
+    @Test
+    fun showProblemWithConnection() {
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/library/get_library_success.json".getJson()))
+
+        onView(withId(R.id.action_search)).perform(click())
+
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(400)
+            .setBody("mockjson/errors/error_400.json".getJson()))
+
+        onView(ViewMatchers.isAssignableFrom(EditText::class.java))
+            .perform(ViewActions.typeText("FIFA"))
+
+        onView(withId(R.id.recyclerListError))
+            .perform(isDisplayed().waitUntil())
+        onView(withId(R.id.recyclerListView))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
+        onView(withId(R.id.errorDescription))
+            .check(ViewAssertions.matches(withText(R.string.load_library_error_not_know)))
+    }
+
+    @Test
+    fun afterClickLoadAgainShouldMakeAnotherRequestAndShowList() {
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/library/get_library_success.json".getJson()))
+
+        onView(withId(R.id.action_search)).perform(click())
+
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(400)
+            .setBody("mockjson/errors/error_400.json".getJson()))
+
+        onView(ViewMatchers.isAssignableFrom(EditText::class.java))
+            .perform(ViewActions.typeText("FIFA"))
+
+        onView(withId(R.id.buttonNameError))
+            .perform(isDisplayed().waitUntil())
+
+        Espresso.pressBack()
+
+        mockWebServer.enqueue(MockResponse()
+            .setResponseCode(200)
+            .setBody("mockjson/searchgame/list_game_success.json".getJson()))
+
+        onView(withId(R.id.buttonNameError))
+            .perform(click())
+
+        onView(withId(R.id.recyclerListView))
+            .perform(isDisplayed().waitUntil())
+        onView(withId(R.id.recyclerListError))
+            .check(ViewAssertions.matches(CoreMatchers.not(isDisplayed())))
     }
 }
--- app/src/debug/java/br/com/concrete/tentacle/testing/SingleFragmentTestActivity.kt
+++ app/src/debug/java/br/com/concrete/tentacle/testing/SingleFragmentTestActivity.kt
@@ -3,14 +3,14 @@ package br.com.concrete.tentacle.testing
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import br.com.concrete.tentacle.R
+import br.com.concrete.tentacle.base.BaseActivity
 
 /**
 * Used for testing fragments inside a fake activity.
 */
-class SingleFragmentTestActivity : AppCompatActivity() {
+class SingleFragmentTestActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val content = FrameLayout(this).apply {
