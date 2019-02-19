46b37ad6975238f7ce9ffdbb81fa2e480db9d1a6
Leonardo Malkes <leo.malkes@gmail.com>
2019-02-19 10:40:23 -0300

    Improvement keyboard behavior in SearchGame

---
+++ app/src/main/java/br/com/concrete/tentacle/extensions/ActivityExt.kt
@@ -0,0 +1,20 @@
+package br.com.concrete.tentacle.extensions
+
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard(){
+    val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    val view = this.currentFocus
+    view?.let {
+        imm.hideSoftInputFromWindow(it.windowToken, 0)
+    }
+}
+
+fun Activity.showKeyboard(){
+    val imm = this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    val view = this.currentFocus
+    view?.let {
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+}
