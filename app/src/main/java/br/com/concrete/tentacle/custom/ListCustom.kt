d68e3d764fe21fa295304b117ce358aa151b6959
Leonardo Malkes <leo.malkes@gmail.com>
2019-02-21 14:43:36 -0300

    BUG FIX - Teste Instrumentado showEmptyCustomLayout

--- app/src/main/java/br/com/concrete/tentacle/custom/ListCustom.kt
+++ app/src/main/java/br/com/concrete/tentacle/custom/ListCustom.kt
@@ -26,6 +26,7 @@ class ListCustom(
     private var errorDescriptionReference: Int = DEFAULT_INVALID_RESOURCE
     private var buttonNameErrorReference: Int = DEFAULT_INVALID_RESOURCE
     private var buttonNameActionReference: Int = DEFAULT_INVALID_RESOURCE
+    private var emptyListMessageReference: Int = DEFAULT_INVALID_RESOURCE
 
     init {
         View.inflate(context, R.layout.list_custom, this)
@@ -40,6 +41,7 @@ class ListCustom(
             errorDescriptionReference = getResourceId(R.styleable.ListCustom_errorDescription, DEFAULT_INVALID_RESOURCE)
             buttonNameErrorReference = getResourceId(R.styleable.ListCustom_buttonNameError, DEFAULT_INVALID_RESOURCE)
             buttonNameActionReference = getResourceId(R.styleable.ListCustom_buttonNameAction, DEFAULT_INVALID_RESOURCE)
+            emptyListMessageReference = getResourceId(R.styleable.ListCustom_emptyListMessage, DEFAULT_INVALID_RESOURCE)
         }
     }
 
@@ -49,7 +51,7 @@ class ListCustom(
             showViewError()
         } else {
             if (elements.isEmpty()) {
-                recyclerListError.setUpComponents(iconReference, errorDescriptionReference, buttonNameActionReference)
+                recyclerListError.setUpComponents(iconReference, emptyListMessageReference, buttonNameActionReference)
                 showViewError()
             } else {
                 showViewSuccess()
