943ff465a6d844c129bf4a72eb00e3b2909d8a8a
diogo ribeiro <dgr@cin.ufpe.br>
2019-02-26 16:35:20 -0300

    Load home image :: assemble image url

--- app/src/main/java/br/com/concrete/tentacle/extensions/ImageExt.kt
+++ app/src/main/java/br/com/concrete/tentacle/extensions/ImageExt.kt
@@ -1,10 +1,25 @@
 package br.com.concrete.tentacle.extensions
 
 import android.widget.ImageView
+import br.com.concrete.tentacle.di.GlideApp
 import com.bumptech.glide.Glide
 
 fun ImageView.loadImage(imageResource: Int) {
-    Glide.with(this)
+    GlideApp.with(this)
         .load(imageResource)
+        .into(this)
+}
+
+fun ImageView.loadImageUrl(imageUrl: String) {
+    GlideApp.with(this)
+        .load(imageUrl)
+        .centerCrop()
+        .into(this)
+}
+
+fun ImageView.loadRoundImageUrl(imageUrl: String) {
+    GlideApp.with(this)
+        .load(imageUrl)
+        .circleCrop()
         .into(this)
 }
--- app/src/main/java/br/com/concrete/tentacle/utils/Utils.kt
+++ app/src/main/java/br/com/concrete/tentacle/utils/Utils.kt
@@ -14,4 +14,8 @@ object Utils {
             .resources
             .assets
             .open(path).bufferedReader().use { it.readText() }
+
+    fun assembleGameImageUrl(sizeType: String, imageId: String, isMaximumType: Boolean) =
+        "https://images.igdb.com/igdb/image/upload/t_$sizeType" +
+                "${if (isMaximumType) IMAGE_SYZE_TYPE_MAXIMUM else EMPTY_STRING}/$imageId.jpg"
 }
