bcefd609ee4cc59b60570191c53ed19a15d143e6
Maherbson Silva Dantas <maherbson@gmail.com>
2019-03-01 14:24:14 -0300

    testes unitários

---
+++ app/src/test/java/br/com/concrete/tentacle/features/myreservations/MyReservationDetailViewModelTest.kt
@@ -0,0 +1,12 @@
+package br.com.concrete.tentacle.features.myreservations
+
+import br.com.concrete.tentacle.base.BaseViewModelTest
+import br.com.concrete.tentacle.features.myreservations.detail.MyReservationDetailViewModel
+import org.junit.Test
+import org.koin.standalone.inject
+
+class MyReservationDetailViewModelTest: BaseViewModelTest()  {
+
+    private val myReservationDetail: MyReservationDetailViewModel by inject()
+
+}
