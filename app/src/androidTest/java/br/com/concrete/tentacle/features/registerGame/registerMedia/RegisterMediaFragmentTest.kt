74f30d9929e78c69746f54987faf2dfa31061e60
Maherbson Silva Dantas <maherbson@gmail.com>
2019-03-08 11:24:48 -0300

    Test instrumented

--- app/src/androidTest/java/br/com/concrete/tentacle/features/registerGame/registerMedia/RegisterMediaFragmentTest.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/features/registerGame/registerMedia/RegisterMediaFragmentTest.kt
@@ -2,84 +2,198 @@ package br.com.concrete.tentacle.features.registerGame.registerMedia
 
 import android.os.Bundle
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.base.BaseFragmentTest
+import br.com.concrete.tentacle.data.models.BaseModel
 import br.com.concrete.tentacle.data.models.Game
 import br.com.concrete.tentacle.extensions.fromJson
 import br.com.concrete.tentacle.extensions.getJson
 import com.google.gson.Gson
 import okhttp3.mockwebserver.MockResponse
-import org.junit.Assert.assertTrue
+import org.junit.Assert
 import org.junit.Test
 
 class RegisterMediaFragmentTest : BaseFragmentTest() {
 
     private val responseJson =
-        "mockjson/loadmygames/game_register_media_success.json".getJson()
+        "mockjson/registerMedia/detail_game_success.json".getJson()
 
-    private val expectedGame = Gson().fromJson<Game>(responseJson)
+    private val mediaSaveSuccess =
+        "mockjson/registerMedia/register_media_success.json".getJson()
+
+    private val mediaAlreadyRegistered =
+        "mockjson/registerMedia/register_media_error_already_registered.json".getJson()
+
+    private val expectedGame = Gson().fromJson<BaseModel<Game>>(responseJson)
 
     override fun setupFragment() {
         testFragment = RegisterMediaFragment().apply {
             val args = Bundle()
-            args.putSerializable("game_argument", expectedGame)
+            args.putSerializable("game_argument", expectedGame.data._id)
             arguments = args
         }
     }
 
     @Test
-    fun showRegisteredGameName() {
-        onView(withId(R.id.mediaRegisterNameTextView))
-            .check(matches(withText(expectedGame.name)))
-    }
+    fun showDetailGame() {
 
-    @Test
-    fun showSnackBarWhenNoPlatformIsSelected() {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(responseJson)
+        )
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(mediaSaveSuccess)
+        )
+
+        onView(withId(R.id.tvGameName))
+            .check(matches(withText("FIFA 06: Road to FIFA World Cup")))
+        onView(withId(R.id.tvGameSummary))
+            .check(matches(withText("You can experience the excitement of international soccer competition in FIFA 06. Extensive coaching options give you the opportunity to take control of all of your players on the pitch and determine their formation and strategy. FIFA 06 offers game modes ranging from a tournament to individual skill games. You can also play with real players from famous international teams from around the world. Multiplayer modes let you challenge other gamers.")))
+        onView(withId(R.id.tvGameName))
+            .check(matches(withText("FIFA 06: Road to FIFA World Cup")))
+        onView(withId(R.id.tvGameReleaseYear))
+            .check(matches(withText("2005")))
+        onView(withText("Sport")).check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Single player")).check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Multiplayer")).check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Co-operative")).check(matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.chipPs3))
+            .check(matches(isDisplayed()))
+            .perform(click())
+        onView(withId(R.id.chipPs4))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chip360))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chipOne))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chipOne))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chip3ds))
+            .check(matches(isDisplayed()))
+
+        onView(withId( R.id.mediaRegisterButton))
+            .perform(scrollTo())
         onView(withId(R.id.mediaRegisterButton))
-            .perform(click())
-
-        onView(withId(com.google.android.material.R.id.snackbar_text))
-            .check(matches(withText(R.string.register_media_no_selection_error)))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.mediaRegisterButton))
+            .check(matches(isEnabled()))
     }
 
     @Test
-    fun registerMediaPlatformError() {
-        onView(withId(R.id.mediaPS4RadioButton))
+    fun registerMedia() {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(responseJson)
+        )
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(mediaSaveSuccess)
+        )
+
+        checkDetails()
+
+        onView(withId( R.id.mediaRegisterButton))
+            .perform(scrollTo())
+        onView(withId(R.id.mediaRegisterButton))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.mediaRegisterButton))
+            .check(matches(isEnabled()))
+        onView(withId( R.id.mediaRegisterButton))
+            .perform(click())
+        onView(withText("Você confirma que FIFA 06: Road to FIFA World Cup existe para a plataforma PS3?"))
+            .check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Confirmar"))
+            .check(matches(ViewMatchers.isDisplayed()))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
             .perform(click())
 
+        Assert.assertTrue(activityRule.activity.isFinishing)
+    }
+
+    @Test
+    fun registerMediaErroMediaAlreadyRegistered() {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(200)
+                .setBody(responseJson)
+        )
         mockWebServer.enqueue(MockResponse()
-            .setBody(Gson().toJson(Throwable()))
-            .setResponseCode(400))
+            .setResponseCode(400)
+            .setBody(mediaAlreadyRegistered))
+
+        checkDetails()
+
+        onView(withId( R.id.mediaRegisterButton))
+            .perform(scrollTo())
 
         onView(withId(R.id.mediaRegisterButton))
-            .perform(click())
-
-        pressBack()
-        onView(withId(com.google.android.material.R.id.snackbar_text))
-            .check(matches(withText(R.string.register_media_generic_error_test)))
-    }
-
-    @Test
-    fun registerMediaPlatformSuccess() {
-
-        onView(withId(R.id.mediaPS4RadioButton))
-            .perform(click())
-
-        val response =
-            "mockjson/loadmygames/register_media_success.json".getJson()
-
-        mockWebServer.enqueue(MockResponse()
-            .setBody(response)
-            .setResponseCode(201))
+            .check(matches(isDisplayed()))
 
         onView(withId(R.id.mediaRegisterButton))
+            .check(matches(isEnabled()))
+
+        onView(withId( R.id.mediaRegisterButton))
+            .perform(click())
+        onView(withText("Você confirma que FIFA 06: Road to FIFA World Cup existe para a plataforma PS3?"))
+            .check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Confirmar"))
+            .check(matches(ViewMatchers.isDisplayed()))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
             .perform(click())
 
-        assertTrue(activityRule.activity.isFinishing)
+        onView(withText("Ops! Parece que já cadastrou esse jogo."))
+            .check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Mídia já cadastrada"))
+            .check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Ok"))
+            .check(matches(ViewMatchers.isDisplayed()))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
+            .perform(click())
+
     }
+
+    private fun checkDetails() {
+        onView(withId(R.id.tvGameName))
+            .check(matches(withText("FIFA 06: Road to FIFA World Cup")))
+        onView(withId(R.id.tvGameSummary))
+            .check(matches(withText("You can experience the excitement of international soccer competition in FIFA 06. Extensive coaching options give you the opportunity to take control of all of your players on the pitch and determine their formation and strategy. FIFA 06 offers game modes ranging from a tournament to individual skill games. You can also play with real players from famous international teams from around the world. Multiplayer modes let you challenge other gamers.")))
+        onView(withId(R.id.tvGameName))
+            .check(matches(withText("FIFA 06: Road to FIFA World Cup")))
+        onView(withId(R.id.tvGameReleaseYear))
+            .check(matches(withText("2005")))
+        onView(withText("Sport")).check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Single player")).check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Multiplayer")).check(matches(ViewMatchers.isDisplayed()))
+        onView(withText("Co-operative")).check(matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.chipPs3))
+            .check(matches(isDisplayed()))
+            .perform(click())
+        onView(withId(R.id.chipPs4))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chip360))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chipOne))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chipOne))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.chip3ds))
+            .check(matches(isDisplayed()))
+    }
+
 }
