78c4a76cf46b0afffaed4b14b8807dbd90875e22
daividvleal <daivid.v.leal@gmail.com>
2019-03-13 14:17:45 -0300

    initial commit

--- app/src/main/java/br/com/concrete/tentacle/base/AppTentacle.kt
+++ app/src/main/java/br/com/concrete/tentacle/base/AppTentacle.kt
@@ -1,7 +1,13 @@
 package br.com.concrete.tentacle.base
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.util.Log
+import android.widget.Toast
 import br.com.concrete.tentacle.BuildConfig
+import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.di.PROPERTY_BASE_URL
 import br.com.concrete.tentacle.di.androidModule
 import br.com.concrete.tentacle.di.networkModule
@@ -9,10 +15,17 @@ import br.com.concrete.tentacle.di.repositoryModule
 import br.com.concrete.tentacle.di.viewModelModule
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.answers.Answers
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
 class AppTentacle : Application() {
+
+    companion object {
+        var TOKEN: String = String()
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -26,5 +39,44 @@ class AppTentacle : Application() {
                 repositoryModule),
 
             extraProperties = mapOf(PROPERTY_BASE_URL to BuildConfig.BASE_URL))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(
+                NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW)
+            )
+        }
+
+        Log.d("TAG", "Subscribing to weather topic")
+        FirebaseMessaging.getInstance().subscribeToTopic("weather")
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.msg_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.msg_subscribe_failed)
+                }
+                Log.d("TAG", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            }
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("TAG", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+                TOKEN = token!!
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d("TAG", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
     }
 }
