9de38dafaddfef25b586290e43dc3fc3bafd40bc
Maherbson Dantas <maherbson@gmail.com>
2019-03-18 18:38:42 -0300

    Implementando status na lista de minhas reservas

--- app/src/main/java/br/com/concrete/tentacle/features/myreservations/MyReservationViewHolder.kt
+++ app/src/main/java/br/com/concrete/tentacle/features/myreservations/MyReservationViewHolder.kt
@@ -1,6 +1,7 @@
 package br.com.concrete.tentacle.features.myreservations
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.data.models.library.loan.LoanResponse
@@ -10,9 +11,11 @@ import br.com.concrete.tentacle.extensions.toDate
 import br.com.concrete.tentacle.utils.IMAGE_SIZE_TYPE_COVER_SMALL
 import br.com.concrete.tentacle.utils.SIMPLE_DATE_OUTPUT_FORMAT
 import br.com.concrete.tentacle.utils.Utils
+import kotlinx.android.synthetic.main.game_view_header_layout.ivGameStatus
 import kotlinx.android.synthetic.main.item_my_reservation.view.dateImage
 import kotlinx.android.synthetic.main.item_my_reservation.view.game_name
 import kotlinx.android.synthetic.main.item_my_reservation.view.game_owner
+import kotlinx.android.synthetic.main.item_my_reservation.view.ivGameStatus
 import kotlinx.android.synthetic.main.item_my_reservation.view.mediaImageView
 import kotlinx.android.synthetic.main.item_my_reservation.view.status
 
@@ -41,14 +44,17 @@ class MyReservationViewHolder(
                     LoanResponse.LoanState.ACTIVE -> {
                         text = loanResponse.estimatedReturnDate?.toDate()?.format(SIMPLE_DATE_OUTPUT_FORMAT) ?: ""
                         visibility = View.VISIBLE
+                        setColorStatus(holder.itemView, R.color.loan_state_active)
                     }
                     LoanResponse.LoanState.PENDING -> {
                         text = holder.itemView.context.getString(R.string.loan_state_pending)
                         visibility = View.GONE
+                        setColorStatus(holder.itemView, R.color.loan_state_pending)
                     }
                     LoanResponse.LoanState.EXPIRED -> {
                         text = holder.itemView.context.getString(R.string.loan_state_expired)
                         visibility = View.GONE
+                        setColorStatus(holder.itemView, R.color.loan_state_expired)
                     }
                 }
 
@@ -61,5 +67,11 @@ class MyReservationViewHolder(
                 loanClick(loanResponse)
             }
         }
+
+        private fun setColorStatus(holder: View, color: Int) {
+            val colorStatus = ContextCompat.getColor(holder.context, color)
+            holder.ivGameStatus.setColorFilter(colorStatus)
+        }
+
     }
 }
