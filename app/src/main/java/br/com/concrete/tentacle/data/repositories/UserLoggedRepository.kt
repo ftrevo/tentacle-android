e03c430989f0ccd9cfacde7c79be545fc4a8b873
Leonardo <Malkes>
2019-03-19 16:51:20 -0300

    Profile Api

---
+++ app/src/main/java/br/com/concrete/tentacle/data/repositories/UserLoggedRepository.kt
@@ -0,0 +1,24 @@
+package br.com.concrete.tentacle.data.repositories
+
+import br.com.concrete.tentacle.data.models.BaseModel
+import br.com.concrete.tentacle.data.models.Session
+import br.com.concrete.tentacle.data.models.User
+import br.com.concrete.tentacle.data.models.UserRequest
+import br.com.concrete.tentacle.data.network.ApiService
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+
+class UserLoggedRepository(private val api: ApiService) {
+
+    fun getProfile(): Observable<BaseModel<User>> {
+        return api.getProfile()
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+    }
+
+    fun updateProfile(userRequest: UserRequest): Observable<BaseModel<Session>> {
+        return api.updateUserProfile(userRequest)
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+    }
+}
