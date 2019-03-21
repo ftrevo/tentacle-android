7492e949982e7f6115736f32314243a237633aea
diogo ribeiro <dgr@cin.ufpe.br>
2019-03-21 15:10:07 -0300

    [Perfil #382] - Teste Instrumentado #383

--- app/src/androidTest/java/br/com/concrete/tentacle/base/BaseFragmentTest.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/base/BaseFragmentTest.kt
@@ -1,9 +1,20 @@
 package br.com.concrete.tentacle.base
 
 import android.Manifest
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
+import br.com.concrete.tentacle.R
+import br.com.concrete.tentacle.extensions.childAtPosition
 import br.com.concrete.tentacle.testing.SingleFragmentTestActivity
+import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Rule
@@ -32,5 +43,30 @@ abstract class BaseFragmentTest : BaseInstrumentedTest() {
         testFragment.let {
             activityRule.activity.setFragment(it)
         }
+    }
+
+    fun setField(textField: String, id: Int) {
+        Espresso.onView(
+            Matchers.allOf(
+                withId(R.id.edt),
+                withId(id)
+                    .childAtPosition(0)
+                    .childAtPosition(1)
+            )
+        ).perform(
+            scrollTo(),
+            replaceText(textField)
+        )
+    }
+
+    fun checkInputField(textField: String, id: Int) {
+        Espresso.onView(
+            Matchers.allOf(
+                withId(R.id.edt),
+                withId(id)
+                    .childAtPosition(0)
+                    .childAtPosition(1)
+            )
+        ).check(matches(withText(textField)))
     }
 }
