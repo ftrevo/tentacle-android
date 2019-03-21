7bd81aba45ed37ff7ff9ce74c162fcd927a488a1
Maherbson Dantas <maherbson@gmail.com>
2019-03-21 16:46:32 -0300

    Alteração na qualidade da imagem

--- app/src/main/java/br/com/concrete/tentacle/utils/Utils.kt
+++ app/src/main/java/br/com/concrete/tentacle/utils/Utils.kt
@@ -19,8 +19,8 @@ object Utils {
         "https://images.igdb.com/igdb/image/upload/t_$sizeType" +
                 "${if (isMaximumType) IMAGE_SIZE_TYPE_MAXIMUM else EMPTY_STRING}/$imageId.jpg"
 
-    fun assembleGameImageUrlYouTube(imageId: String) =
-            "https://img.youtube.com/vi/$imageId/maxresdefault.jpg"
+    fun assembleGameImageUrlYouTube(imageId: String, qualityImage: String = "mqdefault") =
+            "https://img.youtube.com/vi/$imageId/$qualityImage.jpg"
 
     fun assembleUrlYouTube(videoId: String) =
         "http://www.youtube.com/watch?v=$videoId"
