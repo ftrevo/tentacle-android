55f39b134d2a8b1bbe5734d03bfb0a9591ce317b
daividvleal <daivid.v.leal@gmail.com>
2019-03-26 18:44:48 -0300

    INITIAL commit instrumented test

--- app/src/androidTest/java/br/com/concrete/tentacle/features/myreservations/MyReservationFragmentTest.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/features/myreservations/MyReservationFragmentTest.kt
@@ -1,17 +1,19 @@
-package br.com.concrete.tentacle.myreservations
+package br.com.concrete.tentacle.features.myreservations
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import br.com.concrete.tentacle.R
 import br.com.concrete.tentacle.base.BaseFragmentTest
+import br.com.concrete.tentacle.extensions.childAtPosition
 import br.com.concrete.tentacle.extensions.getJson
 import br.com.concrete.tentacle.extensions.waitUntil
-import br.com.concrete.tentacle.features.myreservations.MyReservationFragment
 import br.com.concrete.tentacle.matchers.RecyclerViewMatcher.Companion.withRecyclerView
 import okhttp3.mockwebserver.MockResponse
 import org.hamcrest.CoreMatchers.not
@@ -147,5 +149,122 @@ class MyReservationFragmentTest : BaseFragmentTest() {
 
         onView(withId(R.id.recyclerListView))
             .perform(isDisplayed().waitUntil())
+    }
+
+    @Test
+    fun longClickDeleteExcluir() {
+        val response = "mockjson/myreservations/load_my_reservations_success.json".getJson()
+        val removeGame = "mockjson/myreservations/load_my_reservations_success_after_remove.json".getJson()
+        setResponse(response, 200)
+        setResponse(removeGame, 200)
+
+        onView(withId(R.id.recyclerListView))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.recyclerListView))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.recyclerListError))
+            .check(matches(not(isDisplayed())))
+        onView(withRecyclerView(R.id.recyclerListView)
+            .atPosition(0))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(withText("Eeee"))))
+        onView(
+            withRecyclerView(R.id.recyclerListView)
+                .atPosition(0))
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.longClick())
+
+        onView(withText("A reserva do jogo Eeee sairá da lista de \"Minhas Reservas\", mas você poderá adicioná-lo novamente no futuro."))
+
+        onView(withText("REMOVER"))
+            .check(matches(isDisplayed()))
+            .inRoot(RootMatchers.isDialog())
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.click())
+
+        onView(withRecyclerView(R.id.recyclerListView)
+            .atPosition(0))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(not(withText("Eeee")))))
+    }
+
+    @Test
+    fun longClickDeleteExcluirNotNow() {
+        val response = "mockjson/myreservations/load_my_reservations_success.json".getJson()
+        setResponse(response, 200)
+
+        onView(withId(R.id.recyclerListView))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.recyclerListView))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.recyclerListError))
+            .check(matches(not(isDisplayed())))
+        onView(withRecyclerView(R.id.recyclerListView)
+            .atPosition(0))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(withText("Eeee"))))
+        onView(
+            withRecyclerView(R.id.recyclerListView)
+                .atPosition(0))
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.longClick())
+
+        onView(withText("A reserva do jogo Eeee sairá da lista de \"Minhas Reservas\", mas você poderá adicioná-lo novamente no futuro."))
+
+        onView(withText("AGORA NÃO"))
+            .check(matches(isDisplayed()))
+            .inRoot(RootMatchers.isDialog())
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.click())
+
+        onView(withRecyclerView(R.id.recyclerListView)
+            .atPosition(0))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant((withText("Eeee")))))
+    }
+
+    //TODO FIX BELLOW METHODS
+    @Test
+    fun testDeleteGameSuccess() {
+        val response = "mockjson/myreservations/load_my_reservations_success.json".getJson()
+        setResponse(response, 200)
+        setResponse("mockjson/library/loan/lend_response_success.json".getJson(), 200)
+        setResponse("mockjson/loadmygames/delete_success.json".getJson(), 200)
+        setResponse("mockjson/loadmygames/load_my_games_success_after_delete.json".getJson(), 200)
+        setResponse("mockjson/loadmygames/load_my_games_success_second.json".getJson(), 200)
+
+        onView(withId(R.id.recyclerListView).childAtPosition(
+            0
+        ).childAtPosition(0)).check(matches(hasDescendant(withText("TEST"))))
+        onView(withId(R.id.recyclerListView).childAtPosition(
+            0
+        ).childAtPosition(0)).perform(click())
+
+        onView(withId(R.id.delete)).perform(click())
+        onView(withText("EXCLUIR")).perform(click())
+        onView(withId(R.id.recyclerListView).childAtPosition(
+            0
+        ).childAtPosition(0)).check(matches(hasDescendant(withText("FIFA 06: Road to FIFA World Cup"))))
+    }
+
+    @Test
+    fun testDeleteGameError() {
+        setResponse("mockjson/loadmygames/load_my_games_success.json".getJson(), 200)
+        setResponse("mockjson/loadmygames/load_my_games_success_second.json".getJson(), 200)
+        setResponse("mockjson/library/loan/lend_response_success.json".getJson(), 200)
+        setResponse("mockjson/errors/error_400.json".getJson(), 400)
+
+        onView(withText("TEST")).perform(click())
+        onView(withId(R.id.delete)).perform(click())
+        onView(withText("EXCLUIR")).perform(click())
+        onView(withText("ERROR MESSAGE.")).check(matches(isDisplayed()))
+    }
+
+    private fun setResponse(json: String, code: Int) {
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(code)
+                .setBody(json)
+        )
     }
 }
---
+++ app/src/androidTest/java/br/com/concrete/tentacle/features/myreservations/detail/TestDeleteMenuButton.kt
@@ -0,0 +1,2 @@
+package br.com.concrete.tentacle.features.myreservations.detail
+
