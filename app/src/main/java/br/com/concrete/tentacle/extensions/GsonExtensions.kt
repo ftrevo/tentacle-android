094f8b49752e5af4ef46588dbc97c2b17a8188e0
Maherbson Dantas <maherbson@gmail.com>
2019-03-27 14:35:07 -0300

    controle de imagens

--- app/src/main/java/br/com/concrete/tentacle/extensions/GsonExtensions.kt
+++ app/src/main/java/br/com/concrete/tentacle/extensions/GsonExtensions.kt
@@ -1,7 +1,10 @@
 package br.com.concrete.tentacle.extensions
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 
 inline fun <reified T> Gson.fromJson(json: String) =
         this.fromJson<T>(json, object : TypeToken<T>() {}.type)
+
+fun Any.toJson() = Gson().toJson(this)!!
