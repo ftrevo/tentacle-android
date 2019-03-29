6237ac059bff84ba96303ad4e58684eb53eece82
diogo ribeiro <dgr@cin.ufpe.br>
2019-03-29 19:29:01 -0300

    Splash screen fix

---
+++ app/src/main/java/br/com/concrete/tentacle/features/Splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package br.com.concrete.tentacle.features.Splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.concrete.tentacle.extensions.launchActivity
+import br.com.concrete.tentacle.features.login.LoginActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        launchActivity<LoginActivity>()
+        finish()
+    }
+
+}
