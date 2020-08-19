ec845cbaadab5eabd88b1d16676e57b7fbb61482
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-19 14:51:00 -0300

    Cleans ForgotPassSendEmail ViewModel tests

--- app/src/test/java/br/com/concrete/tentacle/features/forgotPassword/sendEmail/ForgotPassSendEmailVMTeste.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/forgotPassword/sendEmail/ForgotPassSendEmailVMTeste.kt
@@ -7,8 +7,7 @@ import br.com.concrete.tentacle.data.models.User
 import br.com.concrete.tentacle.data.models.ViewStateModel
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
-import okhttp3.mockwebserver.MockResponse
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.koin.standalone.inject
 
@@ -18,77 +17,84 @@ class ForgotPassSendEmailVMTeste : BaseViewModelTest() {
 
     @Test
     fun `when forgotPassSendEmailViewModel calls forgotPassword should return a user`() {
-        val responseJson = getJson("mockjson/forgotPassword/sendEmail/user.json")
 
+        // arrange
+        val responseJson = getJson("mockjson/forgotPassword/sendEmail/user.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<User>>() {}.type
         val responseObject: BaseModel<User> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
 
         val expected =
-                ViewStateModel(status = ViewStateModel.Status.SUCCESS,
-                    model = responseObject.data)
+            ViewStateModel(
+                status = ViewStateModel.Status.SUCCESS,
+                model = responseObject.data
+            )
+
         var actual = ViewStateModel<User>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
         forgotPassSendEmailViewModel.getStateModel().observeForever {
             actual = ViewStateModel(model = it.model, status = it.status)
         }
 
+        // act
         forgotPassSendEmailViewModel.forgotPassword("email")
-        Assert.assertEquals(expected, actual)
+
+        // assert
+        assertEquals(expected, actual)
     }
 
     @Test
     fun `when forgotPassSendEmailViewModel calls forgotPassword should return a message not found`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_404.json"
-        )
 
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_404.json")
+        mockResponseError404()
         val responseObject: ErrorResponse =
             GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
 
         val expected =
-                ViewStateModel(status = ViewStateModel.Status.ERROR,
-                    model = null, errors = responseObject)
+            ViewStateModel(
+                status = ViewStateModel.Status.ERROR,
+                model = null, errors = responseObject
+            )
+
         var actual = ViewStateModel<User>(status = ViewStateModel.Status.LOADING)
-
-        mockResponseError404()
-
         forgotPassSendEmailViewModel.getStateModel().observeForever {
             actual = ViewStateModel(model = it.model, status = it.status, errors = it.errors)
         }
 
+        // act
         forgotPassSendEmailViewModel.forgotPassword("email")
-        Assert.assertEquals(expected, actual)
+
+        // assert
+        assertEquals(expected, actual)
     }
 
     @Test
     fun `when forgotPassSendEmailViewModel calls forgotPassword should return a message not found error 400`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
 
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
+        mockResponseError400()
         val responseObject: ErrorResponse =
             GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
 
         val expected =
-                ViewStateModel(status = ViewStateModel.Status.ERROR,
-                    model = null, errors = responseObject)
+            ViewStateModel(
+                status = ViewStateModel.Status.ERROR,
+                model = null, errors = responseObject
+            )
+
         var actual = ViewStateModel<User>(status = ViewStateModel.Status.LOADING)
-
-        mockResponseError400()
-
         forgotPassSendEmailViewModel.getStateModel().observeForever {
             actual = ViewStateModel(model = it.model, status = it.status, errors = it.errors)
         }
 
+        // act
         forgotPassSendEmailViewModel.forgotPassword("email")
-        Assert.assertEquals(expected, actual)
+
+        // assert
+        assertEquals(expected, actual)
     }
 }
