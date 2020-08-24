b2469751421d2779faed43c1532ccd5e5e72b639
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-24 11:39:25 -0300

    LoginViewModel Tests refactor

--- app/src/test/java/br/com/concrete/tentacle/features/login/LoginVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/login/LoginVMTest.kt
@@ -9,86 +9,88 @@ import br.com.concrete.tentacle.data.models.ViewStateModel
 import br.com.concrete.tentacle.features.login.LoginViewModel
 import com.google.common.reflect.TypeToken
 import com.google.gson.GsonBuilder
-import okhttp3.mockwebserver.MockResponse
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.koin.standalone.inject
 
 class LoginVMTest : BaseViewModelTest() {
 
     private val loginViewMock: LoginViewModel by inject()
+    lateinit var actual: ViewStateModel<Session>
+
+    @Before
+    fun initializeValidationVariableAsLoading() {
+        actual = ViewStateModel(status = ViewStateModel.Status.LOADING)
+    }
 
     @Test
-    fun `when LoginViewModel calls login should return error message for 401`() {
-        val error = ErrorResponse()
-        error.statusCode = 401
-        error.messageInt.add(R.string.user_or_password_error)
+    fun `givenError401Response whenLogin shouldReturnError401Message`() {
+        // arrange
+        mockResponseError401()
+        val error = ErrorResponse().apply {
+            statusCode = 401
+            messageInt.add(R.string.user_or_password_error)
+        }
         val expected =
             ViewStateModel<Session>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = error)
-        var actual = ViewStateModel<Session>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(401)
-
-        mockServer.enqueue(mockResponse)
-
+                status = ViewStateModel.Status.ERROR, model = null, errors = error
+            )
         loginViewMock.getStateModel().observeForever {
             actual = it.peekContent()
         }
+
+        // act
         loginViewMock.loginUser("daivid.v.leal@concrete.com.br", "123456")
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when LoginViewModel calls login should return error message for 400`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
+    fun `givenError400Response whenLogin shouldReturnError400Message`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         val responseObject: ErrorResponse =
             GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-
+        mockResponseError400()
         val expected =
             ViewStateModel<Session>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
-        var actual = ViewStateModel<Session>(status = ViewStateModel.Status.LOADING)
-
-        mockResponseError400()
-
+                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject
+            )
         loginViewMock.getStateModel().observeForever {
             actual = it.peekContent()
         }
+
+        // act
         loginViewMock.loginUser("daivid.v.leal@concrete.com.br", "123456")
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
     fun `when LoginViewModel calls login should return success`() {
-        val responseJson = getJson(
-            "mockjson/login/login_success.json"
-            )
-        mockServer.enqueue(MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson))
-        mockServer.enqueue(MockResponse()
-            .setResponseCode(200)
-            .setBody(getJson("mockjson/token/token_update_success.json")))
-
+        // arrange
+        val responseJson = getJson("mockjson/login/login_success.json")
+        mockResponse200(responseJson)
+        mockResponse200(getJson("mockjson/token/token_update_success.json"))
         val collectionType = object : TypeToken<BaseModel<Session>>() {}.type
         val responseObject: BaseModel<Session> =
             GsonBuilder().create().fromJson(responseJson, collectionType)
-
         val expected =
             ViewStateModel(
                 status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data)
-        var actual = ViewStateModel<Session>(status = ViewStateModel.Status.LOADING)
-
+                model = responseObject.data
+            )
         loginViewMock.getStateModel().observeForever {
             actual = it.peekContent()
         }
+
+        // act
         loginViewMock.loginUser("daivid.v.leal@concrete.com.br", "123456")
+
+        // assert
         assertEquals(expected, actual)
     }
 }
