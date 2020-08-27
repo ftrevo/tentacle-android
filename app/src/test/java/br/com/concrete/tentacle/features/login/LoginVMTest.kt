4fc6af2748a7e4577c756d565802bc523dcc3c01
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-27 17:57:21 -0300

    Renames Login ViewModel methods and code fixes

--- app/src/test/java/br/com/concrete/tentacle/features/login/LoginVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/login/LoginVMTest.kt
@@ -32,10 +32,10 @@ class LoginVMTest : BaseViewModelTest() {
             statusCode = 401
             messageInt.add(R.string.user_or_password_error)
         }
-        val expected =
-            ViewStateModel<Session>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = error
-            )
+        val expected = ViewStateModel<Session>(
+            status = ViewStateModel.Status.ERROR,
+            model = null, errors = error
+        )
         loginViewMock.getStateModel().observeForever {
             actual = it.peekContent()
         }
@@ -48,16 +48,18 @@ class LoginVMTest : BaseViewModelTest() {
     }
 
     @Test
-    fun `givenError400Response whenLogin shouldReturnError400Message`() {
+    fun `givenErrorResponse whenLogin shouldReturnErrorState`() {
         // arrange
         val responseJson = getJson("mockjson/errors/error_400.json")
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
         mockResponseError400()
-        val expected =
-            ViewStateModel<Session>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject
-            )
+        val expected = ViewStateModel<Session>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
         loginViewMock.getStateModel().observeForever {
             actual = it.peekContent()
         }
@@ -70,19 +72,19 @@ class LoginVMTest : BaseViewModelTest() {
     }
 
     @Test
-    fun `givenSuccessResponse whenLogin shouldReturnSuccess`() {
+    fun `givenSuccessResponse whenLogin shouldReturnSuccessState`() {
         // arrange
         val responseJson = getJson("mockjson/login/login_success.json")
         mockResponse200(responseJson)
         mockResponse200(getJson("mockjson/token/token_update_success.json"))
         val collectionType = object : TypeToken<BaseModel<Session>>() {}.type
-        val responseObject: BaseModel<Session> =
-            GsonBuilder().create().fromJson(responseJson, collectionType)
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data
-            )
+        val responseObject: BaseModel<Session> = GsonBuilder()
+            .create()
+            .fromJson(responseJson, collectionType)
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data
+        )
         loginViewMock.getStateModel().observeForever {
             actual = it.peekContent()
         }
