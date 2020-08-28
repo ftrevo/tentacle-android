22f32c42ed21d158ad9baf55c6c75209e8411f1d
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-28 10:47:41 -0300

    Fixes LendGame ViewModel tests

--- app/src/test/java/br/com/concrete/tentacle/features/library/loan/LendGameVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/library/loan/LendGameVMTest.kt
@@ -11,7 +11,6 @@ import br.com.concrete.tentacle.features.lendgame.LendGameViewModel
 import br.com.concrete.tentacle.utils.LOAN_ACTION_LEND
 import com.google.common.reflect.TypeToken
 import com.google.gson.GsonBuilder
-import okhttp3.mockwebserver.MockResponse
 import org.junit.Assert
 import org.junit.Test
 import org.koin.standalone.inject
@@ -21,116 +20,104 @@ class LendGameVMTest : BaseViewModelTest() {
     private val lendGameViewModel: LendGameViewModel by inject()
 
     @Test
-    fun `when loanViewModel calls fetchMediaLoan should return a Media`() {
-
-        val responseJson = getJson(
-            "mockjson/library/loan/lend_response_success.json"
-        )
-
+    fun `givenSuccessfulResponse whenFetchMediaLoan shouldReturnAMedia`() {
+        // arrange
+        val responseJson = getJson("mockjson/library/loan/lend_response_success.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<Media>>() {}.type
         val responseObject: BaseModel<Media> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data
+        )
         var actual = ViewStateModel<Media>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
         lendGameViewModel.getMediaViewState().observeForever {
             actual = it
         }
 
+        // act
         lendGameViewModel.fetchMediaLoan("someId")
+
+        //assert
         Assert.assertEquals(expected, actual)
     }
 
     @Test
-    fun `when lendGameViewModel calls fetchMediaLoan should return error message for 400`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-        responseObject.statusCode = 400
-        val expected =
-            ViewStateModel<Media>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
-        var actual = ViewStateModel<Media>(status = ViewStateModel.Status.LOADING)
-
+    fun `givenErrorResponse whenFetchMediaLoan shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         mockResponseError400()
-
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        responseObject.statusCode = 400
+        val expected = ViewStateModel<Media>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<Media>(status = ViewStateModel.Status.LOADING)
         lendGameViewModel.getMediaViewState().observeForever {
             actual = it
         }
 
+        // act
         lendGameViewModel.fetchMediaLoan("someId")
+
+        // assert
         Assert.assertEquals(expected, actual)
     }
 
     @Test
-    fun `when lendGameViewModel calls updateMediaLoan should return a LoanResponse`() {
-
-        val responseJson = getJson(
-            "mockjson/library/loan/perform_loan_success.json"
-        )
-
+    fun `givenSuccessResponse whenUpdateMediaLoan shouldReturnALoanResponse`() {
+        // arrange
+        val responseJson = getJson("mockjson/library/loan/perform_loan_success.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<LoanResponse>>() {}.type
         val responseObject: BaseModel<LoanResponse> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data
+        )
         var actual = ViewStateModel<LoanResponse>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
         lendGameViewModel.getUpdateLoanViewState().observeForever {
             actual = it
         }
 
+        // act
         lendGameViewModel.updateMediaLoan("someId", LoanActionRequest(LOAN_ACTION_LEND))
+
+        // assert
         Assert.assertEquals(expected, actual)
     }
 
     @Test
-    fun `when lendGameViewModel calls performLoan should return error message for 400`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-        responseObject.statusCode = 400
-        val expected =
-            ViewStateModel<LoanResponse>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
-        var actual = ViewStateModel<LoanResponse>(status = ViewStateModel.Status.LOADING)
-
+    fun `givenErrorResponse whenPerformLoan shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         mockResponseError400()
-
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        responseObject.statusCode = 400
+        val expected = ViewStateModel<LoanResponse>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<LoanResponse>(status = ViewStateModel.Status.LOADING)
         lendGameViewModel.getUpdateLoanViewState().observeForever {
             actual = it
         }
 
+        // act
         lendGameViewModel.updateMediaLoan("someId", LoanActionRequest(LOAN_ACTION_LEND))
+
+        // assert
         Assert.assertEquals(expected, actual)
     }
 }
