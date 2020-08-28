deeaef3f154aaf79ea4992916f338bbe8288ba14
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-28 10:35:17 -0300

    Cleans Home ViewModel Tests

--- app/src/test/java/br/com/concrete/tentacle/features/home/HomeVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/home/HomeVMTest.kt
@@ -11,64 +11,65 @@ import com.google.gson.reflect.TypeToken
 import okhttp3.mockwebserver.MockResponse
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
 import org.koin.standalone.inject
 
 class HomeVMTest : BaseViewModelTest() {
 
     val homeViewModel by inject<HomeViewModel>()
+    lateinit var actual: ViewStateModel<ArrayList<Game>>
+
+    @BeforeEach
+    fun initializeValidationVariable(){
+        actual = ViewStateModel(status = ViewStateModel.Status.LOADING)
+    }
 
     @Test
-    fun `when HomeViewModel calls getHomeGames should return error message for 400`() {
-
+    fun `givenErrorResponse whenGetHomeGames shouldReturnErrorStatus`() {
         // arrange
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
+        val responseJson = getJson("mockjson/errors/error_404.json")
+        mockResponseError404()
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        val expected = ViewStateModel<ArrayList<Game>>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
         )
-        mockResponseError400()
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-
-        val expected =
-            ViewStateModel<ArrayList<Game>>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
-
-        var actual = ViewStateModel<ArrayList<Game>>(status = ViewStateModel.Status.LOADING)
         homeViewModel.getHomeGames().observeForever {
             actual = ViewStateModel(
                 model = it.peekContent().model as? ArrayList<Game>?,
                 status = it.peekContent().status,
-                errors = it.peekContent().errors)
+                errors = it.peekContent().errors
+            )
         }
 
         // act
         homeViewModel.loadHomeGames()
 
         //assert
-        assertEquals(expected.status, actual.status)
+        assertEquals(expected, actual)
     }
 
     @Test
-    fun `when HomeViewModel calls getHomeGames should return success`() {
-
+    fun `givenSuccessfulResponse whenGetHomeGames shouldReturnSuccessStatus`() {
         // arrange
-        val responseJson = getJson(
-            "mockjson/home/new_home_games_success.json"
-        )
+        val responseJson = getJson("mockjson/home/new_home_games_success.json")
         mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<GameResponse>>() {}.type
-        val responseObject: BaseModel<GameResponse> =
-            GsonBuilder().create().fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data.list)
-
-        var actual = ViewStateModel<ArrayList<Game>>(status = ViewStateModel.Status.LOADING)
+        val responseObject: BaseModel<GameResponse> = GsonBuilder()
+            .create()
+            .fromJson(responseJson, collectionType)
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data.list
+        )
         homeViewModel.getHomeGames().observeForever {
             actual = ViewStateModel(
                 model = it.peekContent().model as ArrayList<Game>?,
-                status = it.peekContent().status)
+                status = it.peekContent().status
+            )
         }
 
         // act
