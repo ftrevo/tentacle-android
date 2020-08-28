2ccd88d56959732170c95b92aa1ada5e45a2e383
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-28 13:42:01 -0300

    Cleans Loan ViewModel tests

--- app/src/test/java/br/com/concrete/tentacle/features/library/loan/LoanVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/library/loan/LoanVMTest.kt
@@ -9,7 +9,6 @@ import br.com.concrete.tentacle.data.models.library.Library
 import br.com.concrete.tentacle.data.models.library.loan.LoanResponse
 import com.google.common.reflect.TypeToken
 import com.google.gson.GsonBuilder
-import okhttp3.mockwebserver.MockResponse
 import org.junit.Assert
 import org.junit.Test
 import org.koin.standalone.inject
@@ -19,143 +18,131 @@ class LoanVMTest : BaseViewModelTest() {
     private val loanViewModel: LoanViewModel by inject()
 
     @Test
-    fun `when loanViewModel calls getLibrary should return a Library`() {
-
-        val responseJson = getJson(
-            "mockjson/library/get_one_library_success.json"
-        )
-
+    fun `givenSuccessfulResponse whenGetLibrary shouldReturnLibrary`() {
+        // arrange
+        val responseJson = getJson("mockjson/library/get_one_library_success.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<Library>>() {}.type
         val responseObject: BaseModel<Library> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data
+        )
         var actual = ViewStateModel<Library>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
         loanViewModel.getLibrary().observeForever {
             actual = it
         }
 
+        // act
         loanViewModel.loadLibrary("someId")
+
+        // assert
         Assert.assertEquals(expected, actual)
     }
 
     @Test
-    fun `when loanViewModel calls loadLibrary should return error message for 400`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-        responseObject.statusCode = 400
-        val expected =
-            ViewStateModel<Library>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
-        var actual = ViewStateModel<Library>(status = ViewStateModel.Status.LOADING)
-
+    fun `givenErrorResponse whenLoadLibrary shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         mockResponseError400()
-
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        responseObject.statusCode = 400
+        val expected = ViewStateModel<Library>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<Library>(status = ViewStateModel.Status.LOADING)
         loanViewModel.getLibrary().observeForever {
             actual = it
         }
+
+        // act
         loanViewModel.loadLibrary("someId")
+
+        // assert
         Assert.assertEquals(expected, actual)
     }
 
     @Test
-    fun `when loanViewModel calls performLoan should return a LoanResponse`() {
-
-        val responseJson = getJson(
-            "mockjson/library/loan/perform_loan_success.json"
-        )
-
+    fun `givenSuccessfulResponse whenPerformLoan shouldReturnLoanResponse`() {
+        // arrange
+        val responseJson = getJson("mockjson/library/loan/perform_loan_success.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<LoanResponse>>() {}.type
         val responseObject: BaseModel<LoanResponse> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data)
-
-        var actual = ViewStateModel<LoanResponse>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
-        loanViewModel.getLoan().observeForever {
-            actual = it
-        }
-
-        loanViewModel.performLoad("someId")
-        Assert.assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `when loanViewModel calls performLoan should return error message for 400`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data
         )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-        responseObject.statusCode = 400
-
-        val expected =
-            ViewStateModel<LoanResponse>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
         var actual = ViewStateModel<LoanResponse>(status = ViewStateModel.Status.LOADING)
-
-        mockResponseError400()
-
         loanViewModel.getLoan().observeForever {
             actual = it
         }
+
+        // act
         loanViewModel.performLoad("someId")
+
+        // assert
         Assert.assertEquals(expected, actual)
     }
 
     @Test
-    fun `when loanViewModel calls getGame should return a detail game`() {
-        val responseJson = getJson("mockjson/registerMedia/detail_game_success.json")
-        var actualDetail = ViewStateModel<Game>(status = ViewStateModel.Status.LOADING)
+    fun `GivenErrorResponse whenPerformLoan shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
+        mockResponseError400()
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        responseObject.statusCode = 400
+        val expected = ViewStateModel<LoanResponse>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<LoanResponse>(status = ViewStateModel.Status.LOADING)
+        loanViewModel.getLoan().observeForever {
+            actual = it
+        }
 
-        val classType = object : com.google.gson.reflect.TypeToken<BaseModel<Game>>() {}.type
+        // act
+        loanViewModel.performLoad("someId")
+
+        // assert
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `givenSuccessfulResponse whenGetGame shouldReturnGameDetail`() {
+        // arrange
+        val responseJson = getJson("mockjson/registerMedia/detail_game_success.json")
+        mockResponse200(responseJson)
+        var actualDetail = ViewStateModel<Game>(
+            status = ViewStateModel.Status.LOADING
+        )
+        val classType = object : TypeToken<BaseModel<Game>>() {}.type
         val baseResponse: BaseModel<Game> = GsonBuilder()
             .create()
             .fromJson(responseJson, classType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = baseResponse.data)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-        mockServer.enqueue(mockResponse)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = baseResponse.data
+        )
         loanViewModel.getGame().observeForever {
             actualDetail = it
         }
 
+        // act
         loanViewModel.getDetailsGame("idGame")
+
+        // assert
         Assert.assertEquals(expected, actualDetail)
     }
 }
