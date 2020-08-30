bfcd69253191f042b8f3151596dbcbfc6f4096bb
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-29 21:58:58 -0300

    Cleans RegisterUser ViewModel tests

--- app/src/test/java/br/com/concrete/tentacle/features/register/RegisterUserVMTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/register/RegisterUserVMTest.kt
@@ -12,7 +12,6 @@ import br.com.concrete.tentacle.data.models.ViewStateModel
 import br.com.concrete.tentacle.features.register.RegisterUserViewModel
 import com.google.common.reflect.TypeToken
 import com.google.gson.GsonBuilder
-import okhttp3.mockwebserver.MockResponse
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.koin.standalone.inject
@@ -22,98 +21,76 @@ class RegisterUserVMTest : BaseViewModelTest() {
     private val registerUserViewModelTest: RegisterUserViewModel by inject()
 
     @Test
-    fun `when registerUserViewModel calls loadCities should return a list of strings`() {
-        val responseJson = getJson(
-            "mockjson/user/get_cities_success.json"
-        )
-
+    fun `givenSuccessfulResponse whenLoadCities shouldReturnListOfCities`() {
+        // arrange
+        val responseJson = getJson("mockjson/user/get_cities_success.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<CityResponse>>() {}.type
         val responseObject: BaseModel<CityResponse> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data.cities)
-            var actual = ViewStateModel<ArrayList<String>>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data.cities
+        )
+        var actual = ViewStateModel<ArrayList<String>>(status = ViewStateModel.Status.LOADING)
         registerUserViewModelTest.getCities().observeForever {
             actual = it
         }
-
         val stateId = "5a107a691592ca19f7b4a860"
+
+        // act
         registerUserViewModelTest.loadCities(stateId)
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
     fun `when registerUserViewModel calls loadStates should return a list of states`() {
-        val responseJson = getJson(
-            "mockjson/user/get_states_success.json"
-        )
-
+        // arrange
+        val responseJson = getJson("mockjson/user/get_states_success.json")
+        mockResponse200(responseJson)
         val collectionType = object : TypeToken<BaseModel<StateResponse>>() {}.type
         val responseObject: BaseModel<StateResponse> = GsonBuilder()
             .create()
             .fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data.list)
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data.list
+        )
         var actual = ViewStateModel<ArrayList<State>>(status = ViewStateModel.Status.LOADING)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
         registerUserViewModelTest.getStates().observeForever {
             actual = it
         }
 
+        // act
         registerUserViewModelTest.loadStates()
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when registerUserViewModel calls registerUser should return a userRegistered`() {
-        val responseJson = getJson(
-            "mockjson/login/login_success.json"
-        )
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-
-        mockServer.enqueue(mockResponse)
-
-        mockServer.enqueue(MockResponse()
-            .setResponseCode(200)
-            .setBody(getJson("mockjson/token/token_update_success.json")))
-
+    fun `givenValidUser whenRegisterUser shouldReturnSuccessState`() {
+        // arrange
+        val responseJson = getJson("mockjson/login/login_success.json")
+        mockResponse200(responseJson)
+        mockResponse200(getJson("mockjson/token/token_update_success.json"))
         val collectionType = object : TypeToken<BaseModel<Session>>() {}.type
-        val responseObject: BaseModel<Session> =
-            GsonBuilder().create().fromJson(responseJson, collectionType)
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = responseObject.data)
+        val responseObject: BaseModel<Session> = GsonBuilder()
+            .create()
+            .fromJson(responseJson, collectionType)
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = responseObject.data
+        )
         var actual = ViewStateModel<Session>(status = ViewStateModel.Status.LOADING)
-
         registerUserViewModelTest.getUser().observeForever {
             actual = it.peekContent()
         }
 
+        // act
         registerUserViewModelTest.registerUser(
             User(
                 name = "daivid",
@@ -124,80 +101,79 @@ class RegisterUserVMTest : BaseViewModelTest() {
                 city = "Recife"
             )
         )
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when registerUserViewModel calls loadCities should error message`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-
-        val expected =
-            ViewStateModel<ArrayList<String>>(
-                status = ViewStateModel.Status.ERROR,
-                errors = responseObject)
-        var actual = ViewStateModel<ArrayList<String>>(status = ViewStateModel.Status.LOADING)
-
+    fun `givenErrorResponse whenLoadCities shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         mockResponseError400()
-
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        val expected = ViewStateModel<ArrayList<String>>(
+            status = ViewStateModel.Status.ERROR,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<ArrayList<String>>(status = ViewStateModel.Status.LOADING)
         registerUserViewModelTest.getCities().observeForever {
             actual = it
         }
-
         val stateId = "5a107a691592ca19f7b4a860"
+
+        // act
         registerUserViewModelTest.loadCities(stateId)
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when registerUserViewModel calls loadStates should return error message`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-
-        val expected =
-            ViewStateModel<ArrayList<State>>(
-                status = ViewStateModel.Status.ERROR,
-                errors = responseObject)
-        var actual = ViewStateModel<ArrayList<State>>(status = ViewStateModel.Status.LOADING)
-
+    fun `givenErrorResponse whenLoadStates shouldReturnErrorViewModelState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         mockResponseError400()
-
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        val expected = ViewStateModel<ArrayList<State>>(
+            status = ViewStateModel.Status.ERROR,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<ArrayList<State>>(status = ViewStateModel.Status.LOADING)
         registerUserViewModelTest.getStates().observeForever {
             actual = it
         }
 
+        // act
         registerUserViewModelTest.loadStates()
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when registerUserViewModel calls registerUser should return error message`() {
-        val responseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(responseJson, ErrorResponse::class.java)
-
-        val expected =
-            ViewStateModel<Session>(
-                status = ViewStateModel.Status.ERROR, model = null, errors = responseObject)
-        var actual = ViewStateModel<Session>(status = ViewStateModel.Status.LOADING)
-
+    fun `givenErrorResponse whenRegisterUser shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
         mockResponseError400()
-
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        val expected = ViewStateModel<Session>(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
+        var actual = ViewStateModel<Session>(status = ViewStateModel.Status.LOADING)
         registerUserViewModelTest.getUser().observeForever {
             actual = it.peekContent()
         }
 
+        // act
         registerUserViewModelTest.registerUser(
             User(
                 name = "daivid",
@@ -208,6 +184,8 @@ class RegisterUserVMTest : BaseViewModelTest() {
                 city = "Recife"
             )
         )
+
+        // assert
         assertEquals(expected, actual)
     }
 }
