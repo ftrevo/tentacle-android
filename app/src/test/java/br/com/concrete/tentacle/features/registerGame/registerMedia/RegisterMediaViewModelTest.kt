a501e32d253735e05644145ad37f847c3476333a
Bernardo Junior <bernardodemsj@gmail.com>
2020-08-29 23:16:38 -0300

    Cleans RegisterMedia ViewModel tests

--- app/src/test/java/br/com/concrete/tentacle/features/registerGame/registerMedia/RegisterMediaViewModelTest.kt
+++ app/src/test/java/br/com/concrete/tentacle/features/registerGame/registerMedia/RegisterMediaViewModelTest.kt
@@ -8,7 +8,6 @@ import br.com.concrete.tentacle.data.models.Media
 import br.com.concrete.tentacle.data.models.ViewStateModel
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
-import okhttp3.mockwebserver.MockResponse
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -18,7 +17,6 @@ class RegisterMediaViewModelTest : BaseViewModelTest() {
 
     private val registerMediaViewModel: RegisterMediaViewModel by inject()
     private lateinit var actual: ViewStateModel<Media>
-
     private lateinit var requestGame: BaseModel<Game>
 
     @Before
@@ -31,133 +29,112 @@ class RegisterMediaViewModelTest : BaseViewModelTest() {
     }
 
     @Test
-    fun `when viewmodel successfully register a game should change status to SUCCESS`() {
+    fun `givenSuccessfulResponse whenRegisterMedia shouldReturnSuccessState`() {
+        // arrange
         val responseJson = getJson("mockjson/registerMedia/register_media_success.json")
-
+        mockResponse200(responseJson)
         val classType = object : TypeToken<BaseModel<Media>>() {}.type
         val baseResponse: BaseModel<Media> = GsonBuilder()
             .create()
             .fromJson(responseJson, classType)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-        mockServer.enqueue(mockResponse)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = baseResponse.data
+        )
         registerMediaViewModel.viewStatusModel.observeForever {
             actual = it.peekContent()
         }
 
+        // act
         registerMediaViewModel.registerMedia(
             "PS3",
             requestGame.data
         )
 
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = baseResponse.data)
-
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when something goes wrong trying to register should change status to ERROR`() {
-        val errorResponseJson = getJson(
-            "mockjson/errors/error_400.json"
-        )
-        val mockResponse = MockResponse()
-            .setResponseCode(400)
-            .setBody(errorResponseJson)
-        mockServer.enqueue(mockResponse)
-
+    fun `givenErrorResponse whenRegisterMedia shouldReturnErrorState`() {
+        // arrange
+        val reponseJson = getJson("mockjson/errors/error_400.json")
+        mockResponseError400()
         val responseObject: ErrorResponse =
             GsonBuilder().create().fromJson(
-                errorResponseJson,
+                reponseJson,
                 ErrorResponse::class.java
             )
-
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.ERROR,
-                model = null,
-                errors = responseObject)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
+        )
         registerMediaViewModel.viewStatusModel.observeForever {
             actual = it.peekContent()
         }
 
+        // act
         registerMediaViewModel.registerMedia(
             "PS3",
             requestGame.data
         )
+
+        // assert
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `when viewmodel successfully detail a game should change status to SUCCESS`() {
+    fun `givenSuccessResponse whenGetDetailsGame shouldReturnSuccessState`() {
+        // arrange
         val responseJson = getJson("mockjson/registerMedia/detail_game_success.json")
-        var actualDetail = ViewStateModel<Game>(status = ViewStateModel.Status.LOADING)
-
+        mockResponse200(responseJson)
         val classType = object : TypeToken<BaseModel<Game>>() {}.type
         val baseResponse: BaseModel<Game> = GsonBuilder()
             .create()
             .fromJson(responseJson, classType)
-
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(responseJson)
-        mockServer.enqueue(mockResponse)
-
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.SUCCESS,
+            model = baseResponse.data
+        )
+        var actual = ViewStateModel<Game>(status = ViewStateModel.Status.LOADING)
         registerMediaViewModel.getDetailGame().observeForever {
-            actualDetail = ViewStateModel(model = it.peekContent().model, status = it.peekContent().status)
+            actual = ViewStateModel(model = it.peekContent().model, status = it.peekContent().status)
         }
 
+        // act
         registerMediaViewModel.getDetailsGame("game_id")
 
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.SUCCESS,
-                model = baseResponse.data)
-
-        assertEquals(expected, actualDetail)
+        // assert
+        assertEquals(expected, actual)
     }
 
     @Test
-    fun `when viewmodel successfully detail a game should change status to ERROR`() {
-        var actualDetail = ViewStateModel<Game>(status = ViewStateModel.Status.LOADING)
-
-        val errorResponseJson = getJson(
-            "mockjson/errors/error_400.json"
+    fun `givenErrorResponse whenGetDetailsGame shouldReturnErrorState`() {
+        // arrange
+        val responseJson = getJson("mockjson/errors/error_400.json")
+        mockResponseError400()
+        val responseObject: ErrorResponse = GsonBuilder()
+            .create()
+            .fromJson(responseJson, ErrorResponse::class.java)
+        val expected = ViewStateModel(
+            status = ViewStateModel.Status.ERROR,
+            model = null,
+            errors = responseObject
         )
-        val mockResponse = MockResponse()
-            .setResponseCode(400)
-            .setBody(errorResponseJson)
-        mockServer.enqueue(mockResponse)
-
-        val responseObject: ErrorResponse =
-            GsonBuilder().create().fromJson(
-                errorResponseJson,
-                ErrorResponse::class.java
-            )
-
+        var actual = ViewStateModel<Game>(status = ViewStateModel.Status.LOADING)
         registerMediaViewModel.getDetailGame().observeForever {
-            actualDetail = ViewStateModel(
+            actual = ViewStateModel(
                 model = null,
                 status = it.peekContent().status,
                 errors = it.peekContent().errors
             )
         }
 
+        // act
         registerMediaViewModel.getDetailsGame("game_id")
 
-        val expected =
-            ViewStateModel(
-                status = ViewStateModel.Status.ERROR,
-                model = null,
-                errors = responseObject
-            )
-
-        assertEquals(expected, actualDetail)
+        // assert
+        assertEquals(expected, actual)
     }
 }
