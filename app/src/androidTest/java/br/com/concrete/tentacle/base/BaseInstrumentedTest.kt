655eec572fc99f01ba54b5d67ec4b2546b3c67c2
Bernardo Junior <bernardodemsj@gmail.com>
2020-09-01 15:16:33 -0300

    Updates BaseInstrumentedTest

--- app/src/androidTest/java/br/com/concrete/tentacle/base/BaseInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/base/BaseInstrumentedTest.kt
@@ -3,6 +3,7 @@ package br.com.concrete.tentacle.base
 import androidx.fragment.app.Fragment
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import br.com.concrete.tentacle.data.repositories.SharedPrefRepositoryContract
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
@@ -34,4 +35,23 @@ abstract class BaseInstrumentedTest : KoinComponent {
         sharedPrefRepositoryContract.removeSession()
         sharedPrefRepositoryContract.removeUser()
     }
+
+    fun mockResponse200(responseJson: String?) = mockResponse(200, responseJson)
+
+    fun mockResponse201(responseJson: String?) = mockResponse(201, responseJson)
+
+    fun mockResponseError400() = mockResponse(400, "mockjson/errors/error_400.json")
+
+    fun mockResponseError401() = mockResponse(401, "mockjson/errors/error_401.json")
+
+    fun mockResponseError404() = mockResponse(404, "mockjson/errors/error_404.json")
+
+
+    private fun mockResponse(responseCode: Int, responseJson: String? = "") {
+        val mockResponse = MockResponse()
+            .setResponseCode(responseCode)
+            .setBody(responseJson)
+        mockWebServer.enqueue(mockResponse)
+    }
+
 }
