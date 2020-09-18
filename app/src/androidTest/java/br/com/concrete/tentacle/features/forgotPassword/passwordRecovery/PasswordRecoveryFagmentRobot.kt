ff072898283111bfac0d1847ec4d6166c86f7904
Bernardo Junior <bernardodemsj@gmail.com>
2020-09-18 09:16:09 -0300

    Adds Robot pattern and code refactor to PasswordRecoveryFagment

---
+++ app/src/androidTest/java/br/com/concrete/tentacle/features/forgotPassword/passwordRecovery/PasswordRecoveryFagmentRobot.kt
@@ -0,0 +1,201 @@
+package br.com.concrete.tentacle.features.forgotPassword.passwordRecovery
+
+import androidx.annotation.StringRes
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import br.com.concrete.tentacle.R
+import br.com.concrete.tentacle.extensions.childAtPosition
+import br.com.concrete.tentacle.extensions.getJson
+import br.com.concrete.tentacle.testing.SingleFragmentTestActivity
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+
+fun arrange(action: PasswordRecoveryArrange.() -> Unit) {
+    PasswordRecoveryArrange().apply(action)
+}
+
+fun act(action: PasswordRecoveryAct.() -> Unit) {
+    PasswordRecoveryAct().apply(action)
+}
+
+fun assert(action: PasswordRecoveryAssert.() -> Unit) {
+    PasswordRecoveryAssert().apply(action)
+}
+
+class PasswordRecoveryArrange {
+
+    fun initFragment(intentRule: IntentsTestRule<SingleFragmentTestActivity>) {
+        val fragment = PasswordRecoveryFragment()
+        intentRule.activity.setFragment(fragment)
+    }
+
+    fun initFragmentWithEmailParameter(intentRule: IntentsTestRule<SingleFragmentTestActivity>){
+        val fragment = PasswordRecoveryFragment.newInstance("test@test.com")
+        intentRule.activity.setFragment(fragment)
+    }
+
+    fun mockErrorResponse(mockWebServer: MockWebServer){
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(400)
+                .setBody("mockjson/errors/error_400.json".getJson())
+        )
+    }
+
+}
+
+class PasswordRecoveryAct {
+
+    fun typeEmail() {
+        typeInputField(R.id.emailEditText, "test@test.com")
+    }
+
+    fun clickRecoveryButton() {
+        onView(withId(R.id.recoveryPasswordButton))
+            .perform(scrollTo(), click())
+    }
+
+    fun typeToken() {
+        typeInputField(R.id.tokenEditText, "AIU8D")
+    }
+
+    fun typeShortPassword() {
+        typeInputField(R.id.newPassEditText, "12345")
+    }
+
+    fun typeShortPasswordConfirmation() {
+        typeInputField(R.id.newPassConfirmationEditText, "12345")
+    }
+
+    fun typeCorrectPassword() {
+        typeInputField(R.id.newPassEditText, "123456")
+    }
+
+    fun typeCorrectPasswordConfirmation() {
+        typeInputField(R.id.newPassConfirmationEditText, "123456")
+    }
+
+    fun typeWrongPasswordConfirmation() {
+        typeInputField(R.id.newPassConfirmationEditText, "1234567")
+    }
+
+    private fun typeInputField(resId: Int, text: String) {
+        onView(
+            withId(resId)
+                .childAtPosition(0)
+                .childAtPosition(1)
+        ).perform(
+            scrollTo(),
+            replaceText(text)
+        )
+    }
+
+}
+
+class PasswordRecoveryAssert {
+
+    fun isEmailTextEmpty() {
+        isFieldEmpty(R.id.emailEditText)
+    }
+
+    fun isEmailTextWithCorrectContent() {
+        onView(withId(R.id.emailEditText))
+            .check(
+                matches(
+                    hasDescendant(withText("test@test.com"))
+                )
+            )
+    }
+
+    fun isTokenEmpty(){
+        isFieldEmpty(R.id.tokenEditText)
+    }
+
+    fun isPasswordEmpty() {
+        isFieldEmpty(R.id.newPassEditText)
+    }
+
+    fun isConfirmPasswordEmpty() {
+        isFieldEmpty(R.id.newPassConfirmationEditText)
+    }
+
+    fun emailFieldErrorIsDisplayed() {
+        errorById(R.id.emailEditText)?.check(
+            matches(isDisplayed())
+        )
+    }
+
+    fun tokenFieldErrorIsDisplayed() {
+        errorById(R.id.tokenEditText)?.check(
+            matches(isDisplayed())
+        )
+    }
+
+    fun newPassFieldErrorIsDisplayed() {
+        errorById(R.id.newPassEditText)?.check(
+            matches(isDisplayed())
+        )
+    }
+
+    fun confirmPassFieldErrorIsDisplayed() {
+        errorById(R.id.newPassConfirmationEditText)?.check(
+            matches(isDisplayed())
+        )
+    }
+
+    fun shortPasswordError() {
+        errorById(R.id.newPassEditText)?.check(
+            matches(
+                withText("O campo password precisa ter no mínimo 6 digitos")
+            )
+        )
+    }
+
+    fun shortConfirmationPasswordError() {
+        errorById(R.id.newPassConfirmationEditText)?.check(
+            matches(
+                withText("O campo password precisa ter no mínimo 6 digitos")
+            )
+        )
+    }
+
+    fun differentPasswordsError() {
+        errorById(R.id.newPassConfirmationEditText)?.check(
+            matches(
+                withText("Os campos senha e comfirmação de senha não coincidem")
+            )
+        )
+    }
+
+    fun genericResponseErrorIsDisplayed() {
+        onView(withText("ERROR MESSAGE."))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun errorById(resId: Int): ViewInteraction? {
+        return onView(
+            withId(resId)
+                .childAtPosition(0)
+                .childAtPosition(2)
+        )
+    }
+
+    private fun isFieldEmpty(@StringRes resId: Int) {
+        onView(withId(resId))
+            .check(
+                matches(
+                    hasDescendant(withText(""))
+                )
+            )
+    }
+
+}
--- app/src/androidTest/java/br/com/concrete/tentacle/features/forgotPassword/passwordRecovery/PasswordRecoveryFragmentTest.kt
+++ app/src/androidTest/java/br/com/concrete/tentacle/features/forgotPassword/passwordRecovery/PasswordRecoveryFragmentTest.kt
@@ -1,138 +1,187 @@
 package br.com.concrete.tentacle.features.forgotPassword.passwordRecovery
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAssertion
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.action.ViewActions.scrollTo
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import br.com.concrete.tentacle.R
-import br.com.concrete.tentacle.base.BaseFragmentTest
-import br.com.concrete.tentacle.extensions.childAtPosition
-import br.com.concrete.tentacle.extensions.getJson
-import okhttp3.mockwebserver.MockResponse
-import org.hamcrest.Matchers.not
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import br.com.concrete.tentacle.testing.SingleFragmentTestActivity
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
-class PasswordRecoveryFragmentTest : BaseFragmentTest() {
+class PasswordRecoveryFragmentTest {
 
-    override fun setupFragment() {
-        val emailTest = "email@email.com"
-        testFragment = PasswordRecoveryFragment.newInstance(emailTest)
+    var mockWebServer = MockWebServer()
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(SingleFragmentTestActivity::class.java)
+
+    @Before
+    fun startServer() {
+        mockWebServer.start(8080)
+    }
+
+    @After
+    fun shutdownServer() {
+        mockWebServer.shutdown()
     }
 
     @Test
-    fun shouldFillEmailInputFieldWhenHasIntentExtras() {
-        onView(withId(R.id.emailEditText))
-            .check(matches(hasDescendant(withText("email@email.com"))))
-    }
-
-    @Test
-    fun shouldShowInputFieldsErrorsWhenNothingIsFilled() {
-        setInputField(R.id.emailEditText, "")
-        performClickButton()
-
-        checkErrorById(R.id.emailEditText) {
-            matches(isDisplayed())
+    fun givenFragmentStartWithNoExtras_whenStartFragment_shouldDisplayItCorrectly() {
+        arrange {
+            initFragment(intentsTestRule)
         }
-
-        checkErrorById(R.id.tokenEditText) {
-            matches(isDisplayed())
-        }
-
-        checkErrorById(R.id.newPassEditText) {
-            matches(isDisplayed())
-        }
-
-        checkErrorById(R.id.newPassConfirmationEditText) {
-            matches(not(isDisplayed()))
+        assert {
+            isEmailTextEmpty()
+            isTokenEmpty()
+            isPasswordEmpty()
+            isConfirmPasswordEmpty()
         }
     }
 
     @Test
-    fun invalidPassword() {
-        setInputField(R.id.tokenEditText, "AIU8D")
-        setInputField(R.id.newPassEditText, "12345")
-        performClickButton()
-
-        checkErrorById(R.id.newPassEditText) {
-            matches(isDisplayed())
+    fun givenEmailPassedAsParameter_whenStartFragment_shouldDisplayItCorrectly() {
+        arrange {
+            initFragmentWithEmailParameter(intentsTestRule)
         }
-        checkErrorById(R.id.newPassEditText) {
-            matches(withText("O campo password precisa ter no mínimo 6 digitos"))
-        }
-    }
-
-    @Test
-    fun invalidConfirmationPassword() {
-        setInputField(R.id.tokenEditText, "AIU8D")
-        setInputField(R.id.newPassEditText, "123456")
-        setInputField(R.id.newPassConfirmationEditText, "12345")
-        performClickButton()
-
-        checkErrorById(R.id.newPassConfirmationEditText) {
-            matches(isDisplayed())
-        }
-        checkErrorById(R.id.newPassConfirmationEditText) {
-            matches(withText("O campo password precisa ter no mínimo 6 digitos"))
+        assert {
+            isEmailTextWithCorrectContent()
+            isTokenEmpty()
+            isPasswordEmpty()
+            isConfirmPasswordEmpty()
         }
     }
 
     @Test
-    fun passwordsDoesNotMatch() {
-        setInputField(R.id.tokenEditText, "AIU8D")
-        setInputField(R.id.newPassEditText, "123456")
-        setInputField(R.id.newPassConfirmationEditText, "123457")
-        performClickButton()
-
-        checkErrorById(R.id.newPassConfirmationEditText) {
-            matches(isDisplayed())
+    fun givenEmptyEmailField_whenRecoveringPassword_shouldDisplayNotValidEmailError() {
+        arrange {
+            initFragment(intentsTestRule)
         }
-        checkErrorById(R.id.newPassConfirmationEditText) {
-            matches(withText("Os campos senha e comfirmação de senha não coincidem"))
+        act {
+            typeToken()
+            typeCorrectPassword()
+            typeCorrectPasswordConfirmation()
+            clickRecoveryButton()
+        }
+        assert {
+            emailFieldErrorIsDisplayed()
         }
     }
 
     @Test
-    fun restorePasswordFailInvalidToken() {
-        mockWebServer.enqueue(
-            MockResponse()
-                .setResponseCode(400)
-                .setBody("mockjson/errors/error_400.json".getJson())
-        )
-
-        setInputField(R.id.tokenEditText, "AIU8D")
-        setInputField(R.id.newPassEditText, "123456")
-        setInputField(R.id.newPassConfirmationEditText, "123456")
-        performClickButton()
-        onView(withText("ERROR MESSAGE.")).check(matches(isDisplayed()))
+    fun givenEmptyTokenField_whenRecoveringaPassword_shouldDisplayNotValidTokenError() {
+        arrange {
+            initFragment(intentsTestRule)
+        }
+        act {
+            typeEmail()
+            typeCorrectPassword()
+            typeCorrectPasswordConfirmation()
+            clickRecoveryButton()
+        }
+        assert {
+            tokenFieldErrorIsDisplayed()
+        }
     }
 
-    private fun setInputField(resId: Int, text: String) {
-        onView(withId(resId)
-                .childAtPosition(0)
-                .childAtPosition(1)
-        ).perform(
-            scrollTo(),
-            replaceText(text)
-        )
+    @Test
+    fun givenEmptyPasswordField_whenRecoveringaPassword_shouldDisplayNotValidPasswordError() {
+        arrange {
+            initFragment(intentsTestRule)
+        }
+        act {
+            typeEmail()
+            typeToken()
+            clickRecoveryButton()
+        }
+        assert {
+            newPassFieldErrorIsDisplayed()
+        }
     }
 
-    private fun checkErrorById(resId: Int, func: () -> ViewAssertion) {
-        onView(withId(resId)
-            .childAtPosition(0)
-            .childAtPosition(2))
-            .check(
-                func()
-            )
+    @Test
+    fun givenEmptyPasswordConfirmationField_whenRecoveringaPassword_shouldDisplayNotValidPasswordConfirmationError() {
+        arrange {
+            initFragment(intentsTestRule)
+        }
+        act {
+            typeEmail()
+            typeToken()
+            typeCorrectPassword()
+            clickRecoveryButton()
+        }
+        assert {
+            confirmPassFieldErrorIsDisplayed()
+        }
     }
 
-    private fun performClickButton() {
-        onView(withId(R.id.recoveryPasswordButton))
-            .perform(scrollTo(), click())
+    @Test
+    fun givenShortPassword_whenRecoveringPassword_shouldDisplayErrorMessage() {
+        arrange {
+            initFragment(intentsTestRule)
+        }
+        act {
+            typeEmail()
+            typeToken()
+            typeShortPassword()
+            clickRecoveryButton()
+        }
+        assert {
+            newPassFieldErrorIsDisplayed()
+            shortPasswordError()
+        }
+    }
+
+    @Test
+    fun givenCorrectPasswordAndShortPasswordConfirmation_whenRecoveringPassword_shouldDisplayConfirmationFieldError() {
+        arrange {
+            initFragment(intentsTestRule)
+        }
+        act {
+            typeEmail()
+            typeToken()
+            typeCorrectPassword()
+            typeShortPasswordConfirmation()
+            clickRecoveryButton()
+        }
+        assert {
+            confirmPassFieldErrorIsDisplayed()
+            shortConfirmationPasswordError()
+        }
+    }
+
+    @Test
+    fun givenDifferentPasswordInputs_whenRecoveringPassword_shouldShowError() {
+        arrange {
+            initFragment(intentsTestRule)
+        }
+        act {
+            typeEmail()
+            typeToken()
+            typeCorrectPassword()
+            typeWrongPasswordConfirmation()
+            clickRecoveryButton()
+        }
+        assert {
+            confirmPassFieldErrorIsDisplayed()
+            differentPasswordsError()
+        }
+    }
+
+    @Test
+    fun givenInvalidToken_whenRecoveringPassword_shouldDisplayErrorMessage() {
+        arrange {
+            initFragment(intentsTestRule)
+            mockErrorResponse(mockWebServer)
+        }
+        act {
+            typeEmail()
+            typeToken()
+            typeCorrectPassword()
+            typeCorrectPasswordConfirmation()
+            clickRecoveryButton()
+        }
+        assert {
+            genericResponseErrorIsDisplayed()
+        }
     }
 }
